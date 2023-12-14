9af11d021f1d1b5230cbb94acae78fe19c81f504
LukeLaptop <luke.g.schwade@gmail.com>
2023-12-13 23:03:18 -0500

    added double click to create circle functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("brushCanvas");
 const ctx = canvas.getContext('2d');
+let isDraw = false;
+let x = 0;
+let y = 0;
 
 function updateMessage(message){
     document.getElementById("mouseCoords").innerHTML = message;
@@ -8,17 +11,28 @@ function updateMessage(message){
 function getMousePosition(canvas, evt){
     let drawframe = canvas.getBoundingClientRect();
     return {
-        x: evt.clientX -drawframe.left,
+        x: evt.clientX - drawframe.left,
         y: evt.clientY - drawframe.top
     }
 
 }
-brushCanvas.addEventListener('mousemove', function(evt){
+canvas.addEventListener('mousemove', function(evt){
     let mousePosition = getMousePosition(canvas, evt);
     let textPosition = 'Mouse position: ' + mousePosition.x + ', ' + mousePosition.y;
     updateMessage(textPosition)
 }, !1);
 
+canvas.addEventListener("dblclick", (evt) => {
+    let mousePosition = getMousePosition(canvas, evt);
+    
+    drawCircle (ctx, mousePosition);
+})
+
+function drawCircle(ctx, mousePosition){
+    ctx.beginPath();
+    ctx.arc (mousePosition.x, mousePosition.y, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+}
 function clearCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
